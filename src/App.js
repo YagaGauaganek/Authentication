a8f3837c80186f866eb50de0b43e0e1a11155354
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-03 16:15:02 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from './Login';
+import LogoutButton from './Logout';
+import Profile from './Profile';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>AUTH-O-MORPH</h1>
+    <LoginButton />
+    <LogoutButton />
+    <Profile />
     </div>
   );
 }
